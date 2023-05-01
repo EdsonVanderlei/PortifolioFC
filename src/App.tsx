06bc47ece57a54001a6fdbe1cab18f295f8e56d1
Edson Vanderlei <edsonvanderlei@outlook.com.br>
2023-05-01 13:10:07 -0300

    updt

--- src/App.tsx
+++ src/App.tsx
@@ -12,41 +12,42 @@ import Contacts from './Components/Wrappers/Contacts/Contacts';
 import About from './Components/Wrappers/about/About';
 import Skill from './Components/Wrappers/Skills/Skill';
 import Footer from './Components/Footer/Footer';
-import  Image  from './Assets/image/image.jpg';
+import Image from './Assets/image/image.jpg';
 import Image2 from './Assets/image/image2.jpg'
 import Image3 from './Assets/image/image3.jpg'
+import './App.css'
 
 function App() {
   React.useEffect(() => {
     AOS.init({
       duration: 1300,
-      offset: 100
-
-
+      offset: 60
     })
     AOS.refresh()
-
   }, [])
 
 
   const projects = [
     {
-      image: <img src={Image}/>,
-      stacks: ['Typescript','React'],
-      title:'Portifólio',
-      describe: 'Este é um pequeno projeto pessoal'
+      image: <img src={Image} />,
+      stacks: ['Typescript', 'React'],
+      title: 'Portifólio',
+      describe: 'Este é um pequeno projeto pessoal',
+      delay: '300'
     },
     {
-      image: <img src={Image2}/>,
+      image: <img src={Image2} />,
       stacks: ['React'],
-      title:'Projeto Dogs',
-      describe: 'Esse foi um projeto pessoal que decidi fazer antes das aulas do curso Origamid, para poder entender quais eram meus pontos fracos, e poder melhorá-los, ainda estou desenvolvendo ele'
+      title: 'Projeto Dogs',
+      describe: 'Esse foi um projeto pessoal que decidi fazer antes das aulas do curso Origamid, para poder entender quais eram meus pontos fracos, e poder melhorá-los, ainda estou desenvolvendo ele',
+      delay: '500'
     },
     {
-      image: <img src={Image3}/>,
-      stacks: ['HTML','CSS', 'JavaScript'],
-      title:'The first Page',
-      describe: 'Este é meu primeiro projeto front-end, é importante ter uma linha do tempo, para termos noção das nossas evoluções'
+      image: <img src={Image3} />,
+      stacks: ['HTML', 'CSS', 'JavaScript'],
+      title: 'The first Page',
+      describe: 'Este é meu primeiro projeto front-end, é importante ter uma linha do tempo, para termos noção das nossas evoluções',
+      delay: '700'
     }
   ]
 
@@ -64,27 +65,22 @@ function App() {
             <div data-aos='fade-down' id='project' className={css.Content_Wrapper}>
               <Title lineWidth={80} expand>projects</Title>
               <div className={css.ProjectContentWrapper}>
-                 {projects.map((item) => <ProjectItem key={item.title} obj={item}/>)}
+                {projects.map((item) => <ProjectItem key={item.title} obj={item} />)}
               </div>
             </div>
             <div data-aos='fade-down' id='skill' className={css.Content_Wrapper} >
               <Title lineWidth={35} expand={false}>skills</Title>
-              <Skill/>
+              <Skill />
             </div>
-
             <div data-aos='fade-down' id='about' className={css.Content_Wrapper} >
               <Title expand={false} lineWidth={35}>sobre-mim</Title>
-              <About/>
+              <About />
             </div>
             <div data-aos='fade-down' id='contact' className={css.Content_Wrapper} >
               <Title expand={false} lineWidth={20}>contacts</Title>
-              <Contacts/>
-
-
-            </div>  
-           
-            <Footer/>
-
+              <Contacts />
+            </div>
+            <Footer />
           </div>
         </div>
       </div>
--- src/Components/SkillItems/SkillItem.tsx
+++ src/Components/SkillItems/SkillItem.tsx
@@ -1,27 +1,26 @@
 import React from 'react'
 import css from './SkillItem.module.css'
 
-const SkillItem = ({items,children} : {
+const SkillItem = ({ items, children }: {
   items: string[],
   children: React.ReactNode
 }) => {
   return (
     <div className={css.Wrapper}>
-        <h5>{children}</h5>
-        <div className={css.items}>
-         {items?.map((item,index) => {
-            if(index !== items.length -1){
-              return (
-                <p key={item}>{item} | </p> 
-                )
-            }
+      <h5>{children}</h5>
+      <div className={css.items}>
+        {items?.map((item, index) => {
+          if (index !== items.length - 1) {
             return (
-              <p key={item}>{item}</p> 
-              )
-         }
-
-         )}
-        </div>
+              <p key={item}>{item} | </p>
+            )
+          }
+          return (
+            <p key={item}>{item}</p>
+          )
+        }
+        )}
+      </div>
     </div>
   )
 }
